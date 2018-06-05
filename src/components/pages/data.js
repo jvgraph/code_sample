de076fc1458dc1365beb8f5592e415f148bf716b
Juan Vargas <vjuan23@gmail.com>
2018-06-04 23:14:48 -0400

    data source

---
+++ src/components/pages/data.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    title: "Bureaux exquisite delightful carefully curated soft power.",
+    image: require("./../../assets/images/posts/one.jpg"),
+    date: "FEBRUARY 10, 2017",
+    author: "lorem ipsum"
+  },
+  {
+    title: "Sharp bureaux sleepy K-pop carefully curated.",
+    image: require("./../../assets/images/posts/two.jpg"),
+    date: "FEBRUARY 2, 2017",
+    author: "lorem ipsum"
+  },
+  {
+    title: "St Moritz uniforms Beams.",
+    image: require("./../../assets/images/posts/three.jpg"),
+    date: "JANUARY 27, 2017",
+    author: "lorem ipsum"
+  },
+  {
+    title: "Artisanal iconic cutting-edge business class.",
+    image: require("./../../assets/images/posts/six.jpg"),
+    date: "JANUARY 12, 2017",
+    author: "lorem ipsum"
+  },
+  {
+    title: "Esse airport veniam ryokan soft power.",
+    image: require("./../../assets/images/posts/four.jpg"),
+    date: "JANUARY 21, 2017",
+    author: "lorem ipsum"
+  },
+  {
+    title: "K-pop extraordinary.",
+    image: require("./../../assets/images/posts/five.jpg"),
+    date: "JANUARY 18, 2017",
+    author: "lorem ipsum"
+  }
+];
+
+export default data;
--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
-
+import React, { Component } from "react";
+import Slider from "react-slick";
 
 export default class CenterMode extends Component {
   render() {
-    const settings = { 
+    const settings = {
       dots: true,
       arrows: false,
       prevArrow: false,
       nextArrow: false,
       autoplay: true,
-      dotsClass: 'slick-dots slick-thumb',
+      dotsClass: "slick-dots slick-thumb",
       infinite: true,
-      speed: 5000,
+      speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       swipe: true,
@@ -21,52 +20,54 @@ export default class CenterMode extends Component {
     return (
       <div className="home--slider">
         <ul className="slides">
-        <Slider {...settings}>
-          <li className="slide--item">
-            <img src={require('./../../assets/images/slides/one.jpg')}  />
-            <div className="slide--content">
+          <Slider {...settings}>
+            <li className="slide--item">
+              <img src={require("./../../assets/images/slides/one.jpg")} />
+              <div className="slide--content">
                 <div className="container">
-                    <h2 className="slide--title">vitae maiores blanditiis ipsum quasi</h2>
-                    <h3 className="slide--date">
-                        October 15, 2017
-                    </h3>
-                    <p className="slide--description">
-                        Vivamus ac lectus a felis semper facilisis nec id sapien. Cras nec facilisis elit.
-                    </p>
+                  <h2 className="slide--title">
+                    vitae maiores blanditiis ipsum quasi
+                  </h2>
+                  <h3 className="slide--date">October 15, 2017</h3>
+                  <p className="slide--description">
+                    Vivamus ac lectus a felis semper facilisis nec id sapien.
+                    Cras nec facilisis elit.
+                  </p>
                 </div>
-            </div>
-          </li>
-          <li className="slide--item">
-            <img src={require('./../../assets/images/slides/two.jpg')}  />
-            <div className="slide--content">
+              </div>
+            </li>
+            <li className="slide--item">
+              <img src={require("./../../assets/images/slides/two.jpg")} />
+              <div className="slide--content">
                 <div className="container">
-                    <h2 className="slide--title">Sed vitae nibh ac mi sagittis pretium id id nibh</h2>
-                    <h3 className="slide--date">
-                        November 6, 2017
-                    </h3>
-                    <p className="slide--description">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    </p>
+                  <h2 className="slide--title">
+                    Sed vitae nibh ac mi sagittis pretium id id nibh
+                  </h2>
+                  <h3 className="slide--date">November 6, 2017</h3>
+                  <p className="slide--description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  </p>
                 </div>
-            </div>
-          </li>
-          <li className="slide--item">
-            <img src={require('./../../assets/images/slides/three.jpg')}  />
-            <div className="slide--content">
+              </div>
+            </li>
+            <li className="slide--item">
+              <img src={require("./../../assets/images/slides/three.jpg")} />
+              <div className="slide--content">
                 <div className="container">
-                    <h2 className="slide--title">Nullam sem est, bibendum et sodales nec</h2>
-                    <h3 className="slide--date">
-                        December 31, 2017
-                    </h3>
-                    <p className="slide--description">
-                        Aut unde illum soluta libero nam accusamus aspernatur, nostrum tenetur.
-                    </p>
+                  <h2 className="slide--title">
+                    Nullam sem est, bibendum et sodales nec
+                  </h2>
+                  <h3 className="slide--date">December 31, 2017</h3>
+                  <p className="slide--description">
+                    Aut unde illum soluta libero nam accusamus aspernatur,
+                    nostrum tenetur.
+                  </p>
                 </div>
-            </div>
-          </li>
-        </Slider>
+              </div>
+            </li>
+          </Slider>
         </ul>
       </div>
-    )
+    );
   }
 }
